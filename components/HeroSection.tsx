c1cd585eb847d05374af5fc7252d89236042341f
Luzaum <luzaumpevepe@gmail.com>
2025-08-12 07:44:11 -0300

    tryrty

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -79,7 +79,7 @@ export const HeroSection = ({ onGetStarted }: HeroSectionProps) => {
       
       {/* Background Pattern */}
       <div className="absolute inset-0 -z-10 opacity-30">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
+        <div className="absolute inset-0 bg-gradient-to-br from-primary/5 to-secondary/5" />
       </div>
     </div>
   );
